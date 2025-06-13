1ddd7839e30bc030f292d7a82cde1953b5fad30e
Hasan Mahmud <163021899+Hasanmahmud000@users.noreply.github.com>
2025-06-13 20:05:56 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,12 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker: Installed');
+self.addEventListener('install', function (event) {
+  console.log('✅ Service Worker Installed');
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  console.log('Service Worker: Activated');
+self.addEventListener('activate', function (event) {
+  console.log('✅ Service Worker Activated');
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function (event) {
+  event.respondWith(fetch(event.request));
 });
