382219aafd8c6e303e5951d6a1d0ffc212e8389c
Hasan Mahmud <163021899+Hasanmahmud000@users.noreply.github.com>
2025-06-17 06:54:48 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,39 @@
-const CACHE_NAME = "criczone-cache-v1";
+const CACHE_NAME = 'criczone-cache-v1';
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install service worker and cache files
-self.addEventListener("install", event => {
+// Install
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate service worker
-self.addEventListener("activate", event => {
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Activate
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
+    caches.keys().then(keys =>
       Promise.all(
-        cacheNames.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
           }
         })
       )
     )
-  );
-});
-
-// Fetch handler
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
   );
 });
